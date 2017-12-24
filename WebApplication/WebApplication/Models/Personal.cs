fff1f6f2daf7c8097acb6408073eec3301117481
mdodson1995 <m.dodson@hotmail.com>
2017-12-23 22:17:25 -0500

    Add Personal Model

---
+++ WebApplication/WebApplication/Models/Personal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Personal
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone No.")]
+        public string PersonalPhoneNumber { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string PersonalEmailAddress { get; set; }
+    }
+}
