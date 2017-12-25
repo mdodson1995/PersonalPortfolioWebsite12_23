ad3b93bb707ce393e29fc1b94750196ecbf23fa5
mdodson1995 <m.dodson@hotmail.com>
2017-12-25 17:37:37 -0500

    Added to Work Model

---
+++ WebApplication/WebApplication/Models/Work.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Work
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Work/Internship")]
+
+        public string WorkName { get; set; }
+
+        [Display(Name = "Location")]
+
+        public string WorkLocation { get; set; }
+
+        [Display(Name = "Description of Work")]
+
+        public string WorkDescription { get; set; }
+    }
+}
