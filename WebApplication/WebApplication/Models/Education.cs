480d62eeb456e73051bb64eba8a0dfa73db39c7c
mdodson1995 <m.dodson@hotmail.com>
2017-12-25 17:10:26 -0500

    Added to Educations Model

---
+++ WebApplication/WebApplication/Models/Education.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Education
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "School")]
+        public string schoolName { get; set; }
+
+        [Display(Name = "Location")]
+        public string schoolLocation { get; set; }
+
+        [Display(Name = "State")]
+        public string schoolState { get; set; }
+
+        [Display(Name = "Degree")]
+        public string schoolDegree { get; set; }
+    }
+}
